693a06904f36735417417f0906ea053dce04e72b
Huyen Vu <huyenvu2210@gmail.com>
2021-08-21 22:17:08 +0700

    formatted date

--- asm1/src/components/StaffListComponent.js
+++ asm1/src/components/StaffListComponent.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css'
-import { Media } from 'reactstrap';
+import { Card, CardBody, CardText, CardTitle } from 'reactstrap';
+import dateFormat from 'dateformat';
 
 class StaffList extends Component {
     constructor(props){
@@ -21,13 +22,19 @@ class StaffList extends Component {
         if ( staff != null ) {
             return (
                 <div>
-                    {staff.name} , 
-                    {staff.doB} ,
-                    {staff.startDate} ,
-                    {staff.annualLeave} ,
-                    {staff.overTime}
+                    <Card>
+                        <CardBody>
+                            <CardTitle tag="h5">{staff.name}</CardTitle>
+                            <CardText>{dateFormat(staff.doB, "dd/mm/yyyy")}</CardText>
+                            <CardText>{dateFormat(staff.startDate, "dd/mm/yyyy")}</CardText>
+                            <CardText>{staff.department.name}</CardText>
+                            <CardText>{staff.annualLeave}</CardText>
+                            <CardText>{staff.overTime}</CardText>
+                        </CardBody>
+                    </Card>
                 </div>
             )
+
         } else {
             return (
                 <div></div>
@@ -40,21 +47,23 @@ class StaffList extends Component {
         const STAFFS = this.props.staffs.map((staff) => {
             return (
                 <div key={staff.id} className="col col-12 col-md-6 col-lg-4">
-                    <Media tag="li" onClick={ () => this.onStaffSelected(staff) }>
-                        <p>{staff.name}</p>
-                    </Media>
+                    <Card tag="li" onClick={ () => this.onStaffSelected(staff) }>
+                        <CardText>{staff.name}</CardText>
+                    </Card>
                 </div>
             )
         })
         return (
-            <div>
-                <Media list className="row">
+            <div className="container">
+                <div className="row">
                     {STAFFS}
-                </Media>
+                </div>
                 <p>Bấm vào tên nhân viên để xem thông tin.</p>
                 
                 <div className="row">
-                    {this.renderStaff(this.state.staffSelected)}
+                    <div className="col-12 col-md-6">
+                        {this.renderStaff(this.state.staffSelected)}
+                    </div>
                 </div>
             </div>
         )
