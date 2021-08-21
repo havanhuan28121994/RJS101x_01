1f50b0f1f623921c021c1584348afa6962b5495c
Huyen Vu <huyenvu2210@gmail.com>
2021-08-21 23:20:12 +0700

    finished asm1 requirements

--- asm1/src/components/StaffListComponent.js
+++ asm1/src/components/StaffListComponent.js
@@ -11,21 +11,26 @@ class StaffList extends Component {
         }
     }
 
+    // Change state staffSelect from null to the selected staff 
     onStaffSelected(staff) {
         this.setState(
             {staffSelected : staff}
         );
     }
 
+    // render staff information in case a staff is selected, return empty div if none is selected
     renderStaff(staff){
         if ( staff != null ) {
             return (
                 <div>
-                    <Card>
+                    <Card className="mb-4">
                         <CardBody>
                             <CardTitle tag="h5">Họ và tên:{staff.name}</CardTitle>
+
+                            {/* Format date to more easy-to-read date format */}
                             <CardText>Ngày sinh: {dateFormat(staff.doB, "dd/mm/yyyy")}</CardText>
                             <CardText>Ngày vào công ty: {dateFormat(staff.startDate, "dd/mm/yyyy")}</CardText>
+                            
                             <CardText>Phòng ban: {staff.department.name}</CardText>
                             <CardText>Số ngày nghỉ còn lại: {staff.annualLeave}</CardText>
                             <CardText>Số ngày đã làm thêm: {staff.overTime}</CardText>
