ce737eb9c3361259c71783880be469f05ac7e1ba
Huyen Vu <huyenvu2210@gmail.com>
2021-08-22 13:52:39 +0700

    added select department to see staff function

--- asm1/src/components/StaffListComponent.js
+++ asm1/src/components/StaffListComponent.js
@@ -7,7 +7,8 @@ class StaffList extends Component {
         super(props);
 
         this.state = {
-            staffSelected : null
+            staffSelected : null,
+            depSelected: null
         }
     }
 
@@ -46,23 +47,76 @@ class StaffList extends Component {
         }
     }
 
+    // Change state staffSelect from null to the selected staff 
+    onDepSelected(dep) {
+        this.setState(
+            {depSelected : dep}
+        );
+    }
+
+    showAll() {
+        this.setState(
+            {depSelected : null}
+        );
+    }
+
+    renderDep(dep){
+        if (dep != null) {
+            return(
+                this.props.staffs.filter((staff) => staff.department.name === this.state.depSelected.name).map((staff) => {
+                    return (
+                        <div key={staff.id} className="col col-12 col-md-6 col-lg-4">
+                            <Card tag="li" onClick={ () => this.onStaffSelected(staff) } className="mt-2 p-1">
+                                <CardText>{staff.name}</CardText>
+                            </Card>
+                        </div>
+                    )
+                })
+            )
+        } else {
+            return(
+                this.props.staffs.map((staff) => {
+                    return (
+                        <div key={staff.id} className="col col-12 col-md-6 col-lg-4">
+                            <Card tag="li" onClick={ () => this.onStaffSelected(staff) } className="mt-2 p-1">
+                                <CardText>{staff.name}</CardText>
+                            </Card>
+                        </div>
+                    )
+                })
+            )
+        }
+    }
+
     render(){
 
-        const STAFFS = this.props.staffs.map((staff) => {
-            return (
-                <div key={staff.id} className="col col-12 col-md-6 col-lg-4">
-                    <Card tag="li" onClick={ () => this.onStaffSelected(staff) } className="mt-2 p-1">
-                        <CardText>{staff.name}</CardText>
+        // return staffs based on selected department
+
+        const DEPS = this.props.departments.map((dep) => {
+            return(
+                <div key={dep.id} className="col col-6 col-md-3 col-lg-2">
+                    <Card tag="li" onClick={ () => this.onDepSelected(dep) } className="mt-2 p-1 mb-3">
+                        <CardText>{dep.name}</CardText>
                     </Card>
                 </div>
             )
         })
+
         return (
             <div className="container">
                 <div className="row">
-                    {STAFFS}
+                    {this.renderDep(this.state.depSelected)} 
                 </div>
                 <p className="mt-3">Bấm vào tên nhân viên để xem thông tin.</p>
+                <p className="mt-3">Bấm vào tên bộ phận để xem nhân viên trực thuộc.</p>
+                <div className="row">
+                    {DEPS}
+                    <div className="col col-6 col-md-3 col-lg-2">
+                        <Card tag="li" onClick={ () => this.showAll() } className="mt-2 p-1 mb-3">
+                            <CardText>Tất cả</CardText>
+                        </Card>
+                    </div>
+                </div>
                 
                 <div className="row">
                     <div className="col-12 col-md-6">
