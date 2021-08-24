d28376e3720983e2891e42c94a536701b8250be6
Huyen Vu <huyenvu2210@gmail.com>
2021-08-24 23:39:57 +0700

    add salary

--- asm1/src/components/SalaryList.js
+++ asm1/src/components/SalaryList.js
@@ -4,6 +4,9 @@ import { STAFFS } from '../shared/staffs';
 
 const SalaryList = () => {
 
+    const basicSalary = 3000000;
+    const overTimeSalary = 200000;
+
     const staffs = STAFFS.map((staff) => {
         return (
             <div key={staff.id} className="col col-12 col-md-6 col-lg-4">
@@ -12,7 +15,7 @@ const SalaryList = () => {
                     <CardText>Mã nhân viên: {staff.id}</CardText>
                     <CardText>Hệ số lương: {staff.salaryScale}</CardText>
                     <CardText>Số giờ làm thêm: {staff.overTime}</CardText>
-                    <CardText>Lương: 40000000</CardText>
+                    <CardText>Lương: {((staff.salaryScale * basicSalary) + (staff.overTime  * overTimeSalary)).toFixed(1)}</CardText>
                 </Card>
             </div>
         )
