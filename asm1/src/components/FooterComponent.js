515a1b077071d287cd0dbdf1d23b52c056fe0e7c
Huyen Vu <huyenvu2210@gmail.com>
2021-08-25 22:20:01 +0700

    styled footer

--- asm1/src/components/FooterComponent.js
+++ asm1/src/components/FooterComponent.js
@@ -2,13 +2,11 @@ import React from "react";
 
 const Footer = (props) => {
   return (
-    <div className="bg-primary">
+    <div className="bg-info mt-4">
       <div className="container">
         <div className="row row-content">
-          <div className="col-12">
-            <h3>Location Information</h3>
-          </div>
-          <div className="col-12 col-sm-4 offset-sm-1">
+          <div className="col-12 col-lg-6">
+            <h3 className="pt-3">Location Information</h3>
             <h5>Our Address</h5>
             <address>
               121, Clear Water Bay Road
@@ -16,36 +14,42 @@ const Footer = (props) => {
               Clear Water Bay, Kowloon
               <br />
               HONG KONG
-              <br />
-              <i className="fa fa-phone"></i>: +852 1234 5678
-              <br />
-              <i className="fa fa-fax"></i>: +852 8765 4321
-              <br />
-              <i className="fa fa-envelope"></i>:{" "}
-              <a href="mailto:confusion@food.net">confusion@food.net</a>
             </address>
           </div>
-          <div className="col-12 col-sm-6 offset-sm-1">
-            <h5>Map of our Location</h5>
-          </div>
-          <div className="col-12 col-sm-11 offset-sm-1">
-            <div className="btn-group" role="group">
+          <div className="col-12 col-lg-6">
+            <h5 className="pb-2 pt-4">Contact us: </h5>
+            <div className="">
+              <div className="btn-group" role="group">
+                <a
+                  role="button"
+                  className="btn btn-secondary"
+                  href="tel:+85212345678"
+                >
+                  <i className="fa fa-phone"></i> Call
+                </a>
+                <a role="button" className="btn btn-primary">
+                  <i className="fa fa-skype"></i> Skype
+                </a>
+                <a
+                  role="button"
+                  className="btn btn-success"
+                  href="mailto:confusion@food.net"
+                >
+                  <i className="fa fa-envelope-o"></i> Email
+                </a>
+              </div>
+            </div>
+            <div>
+              <i className="fa fa-phone pt-3"></i> +852 1234 5678
+              <br />
+              <i className="fa fa-fax"></i> +852 8765 4321
+              <br />
+              <i className="fa fa-envelope"></i>
               <a
-                role="button"
-                className="btn btn-primary"
-                href="tel:+85212345678"
-              >
-                <i className="fa fa-phone"></i> Call
-              </a>
-              <a role="button" className="btn btn-info">
-                <i className="fa fa-skype"></i> Skype
-              </a>
-              <a
-                role="button"
-                className="btn btn-success"
                 href="mailto:confusion@food.net"
-              >
-                <i className="fa fa-envelope-o"></i> Email
+                style={{ textDecoration: "none", color: "black" }}
+                >
+                confusion@food.net
               </a>
             </div>
           </div>
@@ -53,6 +57,6 @@ const Footer = (props) => {
       </div>
     </div>
   );
-}
+};
 
 export default Footer;
