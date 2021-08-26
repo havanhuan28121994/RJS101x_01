85001044f3c03202f5b6cf879d55e61eb8168a34
Huyen Vu <huyenvu2210@gmail.com>
2021-08-26 14:15:47 +0700

    added sort function to salarylist

--- asm1/src/components/SalaryList.js
+++ asm1/src/components/SalaryList.js
@@ -1,20 +1,125 @@
-import React from "react";
-import { Card, CardText, CardTitle, Breadcrumb, BreadcrumbItem } from "reactstrap";
-import { Link } from 'react-router-dom';
+import React, { Component, useEffect, useState } from "react";
+import {
+  Card,
+  CardText,
+  CardTitle,
+  Breadcrumb,
+  BreadcrumbItem,
+} from "reactstrap";
+import { Link } from "react-router-dom";
 
-const SalaryList = ({staffs}) => {
-    const basicSalary = 3000000;
-    const overTimeSalary = 200000;
+const basicSalary = 3000000;
+const overTimeSalary = 200000;
 
-    const STAFFS = staffs.map((staff) => {
+// function to compare salary to sort staff array
+function compare(a, b) {
+  const salaryA = a.salaryScale * basicSalary + a.overTime * overTimeSalary;
+  const salaryB = b.salaryScale * basicSalary + b.overTime * overTimeSalary;
+
+  let comparison = 0;
+  if (salaryA < salaryB) {
+      comparison = -1;
+    } else if (salaryA > salaryB) {
+      comparison = 1;
+    }
+    return comparison;
+  }
+
+class SalaryList extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      sort: null
+    }
+  }
+
+  // set sort to sort the staff array
+  setSort(sort){
+    this.setState(
+      {sort: sort}
+    )
+  }
+
+  // render sorted array when sort is not null
+  rendersalary(sort) {
+    console.log(sort);
+    if (sort === 1) {
+      return this.props.staffs.sort(compare).map((staff) => {
+        return (
+          <div key={staff.id} className="col col-12 col-md-6 col-lg-4 p-2">
+            <Card tag="li" className="mt-2 p-1">
+              <CardTitle>{staff.name}</CardTitle>
+              <CardText className="pl-2 pb-2">
+                Mã nhân viên: {staff.id}
+              </CardText>
+              <CardText className="pl-2 pb-2">
+                Hệ số lương: {staff.salaryScale}
+              </CardText>
+              <CardText className="pl-2 pb-2">
+                Số giờ làm thêm: {staff.overTime}
+              </CardText>
+              <CardText
+                className="pl-3 pb-2 bg-light"
+                style={{ borderTop: "1px solid #878787" }}
+              >
+                Lương: {(
+                  staff.salaryScale * basicSalary +
+                  staff.overTime * overTimeSalary
+                ).toFixed(1)}
+              </CardText>
+            </Card>
+          </div>
+        );
+      });
+    } else if (sort === 2) {
+      return this.props.staffs.sort(compare).reverse().map((staff) => {
+        return (
+          <div key={staff.id} className="col col-12 col-md-6 col-lg-4 p-2">
+            <Card tag="li" className="mt-2 p-1">
+              <CardTitle>{staff.name}</CardTitle>
+              <CardText className="pl-2 pb-2">Mã nhân viên: {staff.id}</CardText>
+              <CardText className="pl-2 pb-2">
+                Hệ số lương: {staff.salaryScale}
+              </CardText>
+              <CardText className="pl-2 pb-2">
+                Số giờ làm thêm: {staff.overTime}
+              </CardText>
+              <CardText
+                className="pl-3 pb-2 bg-light"
+                style={{ borderTop: "1px solid #878787" }}
+              >
+                Lương: {(
+                  staff.salaryScale * basicSalary +
+                  staff.overTime * overTimeSalary
+                ).toFixed(1)}
+              </CardText>
+            </Card>
+          </div>
+        );
+      });
+    }
+  };
+
+  render() {
+
+    // render unsort staff array when sort = null
+    const STAFFS = this.props.staffs.map((staff) => {
       return (
         <div key={staff.id} className="col col-12 col-md-6 col-lg-4 p-2">
           <Card tag="li" className="mt-2 p-1">
             <CardTitle>{staff.name}</CardTitle>
             <CardText className="pl-2 pb-2">Mã nhân viên: {staff.id}</CardText>
-            <CardText className="pl-2 pb-2">Hệ số lương: {staff.salaryScale}</CardText>
-            <CardText className="pl-2 pb-2">Số giờ làm thêm: {staff.overTime}</CardText>
-            <CardText className="pl-3 pb-2 bg-light" style={{ borderTop : '1px solid #878787'}}>
+            <CardText className="pl-2 pb-2">
+              Hệ số lương: {staff.salaryScale}
+            </CardText>
+            <CardText className="pl-2 pb-2">
+              Số giờ làm thêm: {staff.overTime}
+            </CardText>
+            <CardText
+              className="pl-3 pb-2 bg-light"
+              style={{ borderTop: "1px solid #878787" }}
+            >
               Lương: {(
                 staff.salaryScale * basicSalary +
                 staff.overTime * overTimeSalary
@@ -28,7 +133,9 @@ const SalaryList = ({staffs}) => {
     return (
       <div className="container">
         <div>
-          <Breadcrumb style={{backgroundColor: "#ffffff" , padding : 0, margin : 0}}>
+          <Breadcrumb
+            style={{ backgroundColor: "#ffffff", padding: 0, margin: 0 }}
+          >
             <BreadcrumbItem>
               <Link to="/">Nhân viên</Link>
             </BreadcrumbItem>
@@ -36,7 +143,26 @@ const SalaryList = ({staffs}) => {
           </Breadcrumb>
         </div>
         <h1 className="pb-3 text-dark">Danh sách bảng lương</h1>
-        <div className="row">{STAFFS}</div>
+        <div>
+          <span>Sắp xếp lương theo</span>
+          <button
+            className="btn btn-info ml-2"
+            onClick={() => {
+              this.setSort(1);
+            }}
+          >
+            Thấp &#8594; cao
+          </button>
+          <button
+            className="btn btn-info ml-2"
+            onClick={() => {
+              this.setSort(2);
+            }}
+          >
+            Cao &#8594; thấp{" "}
+          </button>
+        </div>
+        <div className="row">{ this.state.sort ? this.rendersalary(this.state.sort) : STAFFS}</div>
         <div className="row">
           <Link to="/" className="col-12 pt-3">
             &#8592; Trở về Danh sách nhân viên
@@ -44,6 +170,7 @@ const SalaryList = ({staffs}) => {
         </div>
       </div>
     );
+  }
 }
 
 export default SalaryList;
