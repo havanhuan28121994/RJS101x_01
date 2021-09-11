4aae44478b9be6eb99a91afad3b60bd9ce5b0e6c
Huyen Vu <huyenvu2210@gmail.com>
2021-09-11 20:01:15 +0700

    add footer

--- lab/src/components/Main.js
+++ lab/src/components/Main.js
@@ -3,6 +3,7 @@ import Menu from './MenuComponent';
 import { DISHES } from '../shared/dishes'
 import DishDetail from './DishDetailComponent';
 import Header from './HeaderComponent';
+import Footer from './FooterComponent';
 
 class Main extends Component {
   constructor(props) {
@@ -26,6 +27,7 @@ class Main extends Component {
             <Header />
             <Menu dishes={this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)}/>
             <DishDetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]}/>
+            <Footer />
         </div>
     );
   }
